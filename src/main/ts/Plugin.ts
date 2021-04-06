5fede6697938991b4dd07f5ba01a45ec37203b9a
DodgeCode <dorefrati@gmail.com>
2021-04-06 10:15:44 +0300

    Ad unit button.

--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -4,9 +4,38 @@ declare const tinymce: TinyMCE;
 
 const setup = (editor: Editor, url: string): void => {
   editor.ui.registry.addButton('tinymce-ad-button', {
-    text: 'tinymce-ad-button button',
+    text: 'Insert ad',
     onAction: () => {
-      editor.setContent('<p>content added from tinymce-ad-button</p>');
+        editor.windowManager.open({
+            title: 'Insert Ad-unit', // The dialog's title - displayed in the dialog header
+            body: {
+              type: 'panel', // The root body type - a Panel or TabPanel
+              items: [ // A list of panel components
+                {
+                  type: 'htmlpanel', // A HTML panel component
+                  html: `<input 
+                            type="text" 
+                            placeholder="ad-unit code"
+                            id="ad_unit_code_input"
+                            value=""
+                        />`
+                }
+              ]
+            },
+            onSubmit() {
+                let ad_unit_code_input = document.getElementById('ad_unit_code_input')
+                if(ad_unit_code_input.value){
+                    editor.insertContent('{{'+ad_unit_code_input.value+'}}')
+                    editor.windowManager.close()
+                }
+            },
+            buttons: [ // A list of footer buttons
+              {
+                type: 'submit',
+                text: 'OK',
+              }
+            ]
+        });
     }
   });
 };
